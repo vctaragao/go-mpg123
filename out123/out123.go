97b9cde0a817f22c4878ca84cdb4c31f1ab251b6
Victor Moraes <vct.aragao@gmail.com>
2023-07-22 19:26:25 -0300

    docs: adding methods documentation

--- out123/out123.go
+++ out123/out123.go
@@ -8,22 +8,15 @@ package out123
 import "C"
 
 import (
-	"errors"
 	"fmt"
 	"unsafe"
 )
-
-var EOF = errors.New("EOF")
 
 // All output encoding formats supported by out123
 // Contains a handle for and out123 enconder instance
 type Encoder struct {
 	handle *C.out123_handle
 }
-
-///////////////////////////
-// DECODER INSTANCE CODE //
-///////////////////////////
 
 // NewEncoder creates a new out123 encoder instance
 func NewEncoder() (*Encoder, error) {
@@ -44,12 +37,21 @@ func (e *Encoder) Delete() {
 	C.out123_del(e.handle)
 }
 
-// returns a string containing the most recent error message corresponding to
+// strerror returns a string containing the most recent error message corresponding to
 // an out123 encoder instance
 func (e *Encoder) strerror() string {
 	return C.GoString(C.out123_strerror(e.handle))
 }
 
+// Start playback with a certain output format
+// It might be a good idea to have audio data handy to feed after this
+// returns with success.
+// Rationale for not taking a pointer to struct mpg123_fmt: This would
+// always force you to deal with that type and needlessly enlarge the
+// shortest possible program.
+// param rate sampling rate
+// param encoding sample encoding (values matching libmpg123 API)
+// param channels number of channels (1 or 2, usually)
 func (e *Encoder) Start(rate int64, channels int, encodings int)  error {
     var err C.int
 	err = C.out123_start(e.handle, C.long(rate), C.int(channels), C.int(encodings))
@@ -59,8 +61,7 @@ func (e *Encoder) Start(rate int64, channels int, encodings int)  error {
 	return nil
 }
 
-
-// GetFormat returns current output format
+// GetFramesize returns encoder framesize
 func (e *Encoder) GetFramesize() (int, error) {
     var err C.int
     var framesize *C.int
@@ -70,11 +71,6 @@ func (e *Encoder) GetFramesize() (int, error) {
     }
 	return int(*framesize), nil
 }
-
-
-/////////////////////////////
-// INPUT AND DECODING CODE //
-/////////////////////////////
 
 // Open initializes a encoder to output the mpg123 data read
 func (e *Encoder) Open(driver, device string) error {
@@ -94,7 +90,7 @@ func (e *Encoder) Open(driver, device string) error {
 	return nil
 }
 
-// Open initializes a encoder to output the mpg123 data read
+// DriverInfo return the driver and device info of the encoder
 func (e *Encoder) DriverInfo() (string, string, error) {
     var cDriver *C.char
 	defer C.free(unsafe.Pointer(cDriver))
@@ -110,7 +106,9 @@ func (e *Encoder) DriverInfo() (string, string, error) {
 	return C.GoString(cDriver), C.GoString(cDevice), nil
 }
 
-// this function should probably be in a utils package
+// EncodeName return the name of enconding
+// param encoding code (enum #mpg123_enc_enum)
+// This function should probably be in a utils package
 // since its dosent need the encoder to exist
 func (e *Encoder) EncodeName(encoding int) (string, error) {
     var cEnconding *C.char
@@ -122,6 +120,12 @@ func (e *Encoder) EncodeName(encoding int) (string, error) {
 	return C.GoString(cEnconding), nil
 }
 
+// Play hand over data for playback and wait in case audio device is busy.
+// So, per default, if you provided a byte count divisible by the PCM frame size,
+// it is an error when less bytes than given are played.
+// param buffer pointer to raw audio data to be played
+// param bytes number of bytes to read from the buffer
+// return number of bytes played (might be less than given, even zero)
 func (e *Encoder) Play(buffer []byte, readyToRead int)  (int) {
     read := C.out123_play(e.handle, unsafe.Pointer(&buffer), C.size_t(readyToRead))
     return int(read)
